2fc4334c6eeb1df2fcecbe6b49049956f03467dd
ChloeCacola <cscorrigan@icloud.com>
2017-03-21 11:49:49 -0400

    force true removed

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("view engine", "handlebars");
 require("./routes/burger-routes.js")(app);
 
 //sync models; run express app
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log("Listening on PORT " + PORT);
 	});
